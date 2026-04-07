8d0e17c53de62e78b0d50e91357df77f88f7207b
Ashwath <ashwath.singh2005@gmail.com>
2026-04-07 18:47:03 -0400

    initial api and main.go file functions

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+// global struct for api which has methods
+type application struct {
+	config config
+	// logger
+	// db driver
+}
+
+// mount
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// Middleware
+	r.Use(middleware.RequestID) // important for rate limiting
+	r.Use(middleware.RealIP) 	// important for analytics, rate-limiting, and tracing
+	r.Use(middleware.Logger)	// optional
+	r.Use(middleware.Recoverer) // for crashes
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	// how to define a method
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("all good"))
+	})
+
+	//	http.ListenAndServe(":3333", r)
+
+	return r
+}
+
+type config struct {
+	addr string // address of the server (the port)
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn string // domain string to connect to db
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+func main() {
+	cfg := config{
+		addr: ":8080",
+		db: dbConfig{},
 
+	}
 
+	api := application {
+		config: cfg,
+	}
+
+}
